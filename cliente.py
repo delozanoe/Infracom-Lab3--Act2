e7b320c803bc19f1b346dba6d7bc7e23beca46c6
delozanoe <47127176+delozanoe@users.noreply.github.com>
2021-04-20 20:04:29 -0500

    DL UDP server

--- cliente.py
+++ cliente.py
@@ -1,16 +1,153 @@
+import datetime
+import os
 import socket
-import sys
+import threading
+import hashlib
+import time
 
-HOST, PORT = "localhost", 9999
-data = " ".join(sys.argv[1:])
+BUFFER = 4096
+lock = threading.Lock()
 
-# SOCK_DGRAM is the socket type to use for UDP sockets
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def cliente(num, last, lock):
+    datosLog = ""
+    console_msgs = []
+    console_msgs.append("Cliente #" + str(num))
 
-# As you can see, there is no connect() call; UDP has no connections.
-# Instead, data is directly sent to the recipient via sendto().
-sock.sendto(bytes(data + "\n", "utf-8"), (HOST, PORT))
-received = str(sock.recv(1024), "utf-8")
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(10)
+    host = "127.0.0.1"
+    host_port = (host, 20001)
+    i = 0
 
-print("Sent:     {}".format(data))
-print("Received: {}".format(received))
+    s.sendto("REQUEST".encode(), host_port)
+    data = s.recvfrom(BUFFER)
+    host_port = (data[1])
+    s.sendto("READY".encode(), host_port)
+
+    console_msgs.append("Listo para recibir")
+    print("Listo para recibir")
+    fTipo = ""
+    while True:
+        data = s.recvfrom(BUFFER)
+        if (data[0].__contains__(b".")):
+            fTipo = data[0].decode()
+            break
+    finT = 0
+
+    timeout=True
+
+    hashR = ""
+    sha1 = hashlib.sha1()
+    fileName = "./Recibidos/" + str(num) + fTipo
+    f = open(fileName, 'wb')
+    console_msgs.append("Recibiendo archivo")
+    print("Recibiendo archivo", num)
+    while True:
+        # print('receiving data...',i)
+        i += 1
+        try:
+            data = s.recvfrom(BUFFER)
+        except:
+            finT = time.time()
+            hashR = "TIMEOUT"
+            timeout=False
+            print("SALGO CON EXCEPCION")
+            break
+
+        if not data[0]:
+            break
+
+        elif (data[0].__contains__(b"FINM")):
+            val = data[0].find(b"FINM")
+            sha1.update(data[0][:val])
+            hashR = data[0][val:]
+            finT = time.time()
+            break
+        else:
+            sha1.update(data[0])
+            f.write(data[0])
+    f.close()
+    console_msgs.append("Archivo recibido")
+    print("ARCHIVO RECIVIDO", num)
+    # Numero de paquetes recibidos
+    datosLog += str(i) + "/"
+
+    if timeout:
+        hashR = hashR[4:].decode()
+    console_msgs.append("Cliente" + str(num) + "Hash Recibido: \n" + str(hashR))
+    console_msgs.append("Cliente" + str(num) + "Hash Calculado:\n" + str(sha1.hexdigest()))
+    print("HASH RECIBIDO", num)
+    notif = ""
+    if (hashR == sha1.hexdigest()):
+        notif = "Exito"
+        console_msgs.append("Archivo recibido Exitosamente")
+    else:
+        notif = "Error"
+        console_msgs.append("El Hash del archivo recibido es diferente del calculado")
+    print("NOTIF")
+    # Notificacion de recepcion
+    console_msgs.append("Envio de notificacion")
+    recepcion = "Cliente " + str(num) + " termino con estado de " + notif
+    datosLog += recepcion + "/"
+
+    # Envio de tiempo
+    datosLog += str(finT) + "/"
+
+    # Mandar Terminate para terminar el servidor en el puerto en que se encuentre
+    datosLog += "TERMINATE/"
+
+    datosLog += "Hash calculado por el cliente: \n" + str(hashR)
+
+    # print(datosLog)
+    s.sendto(datosLog.encode(), host_port)
+    print("ENVIO DATOS")
+
+    logDatosCliente(recepcion, i, hashR, sha1.hexdigest(), fileName)
+
+    for i in console_msgs:
+        print(i)
+
+
+def createLog():
+    print("Creando log")
+
+    # Fecha y hora --creacion log
+    fecha = datetime.datetime.now()
+
+    logName = "./Logs/Logs-client/" + str(fecha.timestamp()) + ".txt"
+    logFile = open(logName, "a")
+    logFile.write("Fecha: " + str(fecha) + "\n")
+
+    logFile.write("----------------------------------------\n")
+
+    logFile.close()
+    return logName
+
+n_clients = 1
+lock = threading.Lock()
+file = createLog()
+
+def logDatosCliente(recepcion, numPaqRecv, hashR, hash, fileName):
+    with lock:
+        # # Nombre del archivo y tamanio
+        fileN = fileName.split("/")
+        fileN = "Nombre del archivo " + fileN[1] + "\n"
+        fSize = os.path.getsize(fileName)
+        size = "Tamanio del archivo: " + str(fSize) + " bytes\n"
+
+        paquetesR = "Numero de paquetes recibidos por el cliente:" + str(numPaqRecv) + "\n"
+        separador = "\n---------------------------------------\n"
+        hash = "\nHASH calculado en el cliente: \n" + hash
+        hashR = "\nHASH calculado en el servidor: \n" + hashR
+        logFile = open(file, "a")
+        logFile.write(fileN + size + recepcion + "\n" + paquetesR + hashR + hash + separador)
+        logFile.close()
+
+
+for i in range(n_clients):
+    if (i == n_clients - 1):
+        t = threading.Thread(target=cliente, args=(i, True, lock))
+        t.start()
+    else:
+        t = threading.Thread(target=cliente, args=(i, False, lock))
+        t.start()
--- servidor.py
+++ servidor.py
@@ -1,129 +1,169 @@
-import socket, hashlib, threading, time, datetime, osas 
+import socket
+import hashlib
+import threading
+import time
+import datetime
+import os
 
-#TODO
 lock = threading.Lock()
-#GLOBAL VARIABLES
-
-attend = 0
-BUFFER = 4096
 
 
-#Varaibles to tranfer
+def pedirDatos():
+    fileName = ""
+    fileT = ""
+    entr = int(input("Ingrese archivo que quiere enviar 1 (100 MB) o 2 (250MB) "))
+    if (entr == 1):
+        fileName = "./files/100MB.txt"
+        fileT = ".txt"
+    elif (entr == 2):
+        fileName = "./files/250MB.txt"
+        fileT = ".txt"
+    entr = int(input("Ingrese el numero de clientes en simultaneo a enviar el archivo "))
+    numClientes = entr
+    return fileName, fileT, numClientes
+
+
+file_data = pedirDatos()
+fileName = file_data[0]
+n_clientes = file_data[2]
+fileT = file_data[1]
+c_clientes = 0
+attend = False
+
+def createLog():
+    print("Creando log")
+
+    # Fecha y hora --creacion log
+    fecha = datetime.datetime.now()
+
+    logName = "Logs/Logs-server/" + str(fecha.timestamp()) + ".txt"
+    logFile = open(logName, "a")
+    logFile.write("Fecha: " + str(fecha) + "\n")
+
+    # Nombre del archivo y tamanio
+    fileN = fileName.split("/")
+    fileN = fileN[2]
+
+    logFile.write("Nombre del archivo: " + fileN + "\n")
+
+    fSize = os.path.getsize(fileName)
+
+    logFile.write("Tamanio del archivo: " + str(fSize) + " bytes\n")
+    logFile.write("----------------------------------------\n")
+
+    logFile.close()
+    return logName
+
+
+# Crear el archivo de log
+logName = createLog()
+
+
+def logDatosCliente(recepcion, tiempo, numPaqEnv, numPaqRecv, hashR, hash):
+    with lock:
+        paquetesE = "Numero de paquetes enviados por el servidor:" + str(numPaqEnv) + "\n"
+        paquetesR = "Numero de paquetes recibidos por el cliente:" + str(numPaqRecv) + "\n"
+        tiempoT = "Tiempo: " + str(tiempo) + " segundos\n"
+        separador = "\n---------------------------------------\n"
+        hash = "\nHASH calculado en el servidor: \n" + hash
+        logFile = open(logName, "a")
+        logFile.write(recepcion + "\n" + paquetesE + paquetesR + tiempoT + hashR + hash + separador)
+        logFile.close()
+
+
 host = ""
 BUFFER = 4096
 
-
-
-#Menu
-def menu():
-    fileName = ""
-    fileToTransfer = ""
-    dataInput = int(input("Select file to send 1 (100 MB) o 2 (250MB"))
-    if(dataInput == 1):
-        fileName = ""
-        fileToTransfer = ".zip"
-    elif(dataInput == 2):
-        fileName = "
-        fileToTransfer =".zip"
-    entr = int(input("How many clients do you want to manage: "))
-    clientsNumb  = entr
-    return fileName, fileToTransfer, clientsNumb
-
-#Data to transfer
-file_data = menu()
-fileName = file_data[0]
-fileToTransfer = file_data[1]
-number_clients = file_data[2]
-connected_customers = 0
-attend = False
-
-
-#Method Server
-def server(port1,dir):
-    port = 20001+port1
+def servidor(port1,dir):
+    global c_clientes
+    global attend
+    port=20001+port1
     s = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-    #Bind to addresss and ip
+    # Bind to address and ip
     s.bind((host, port))
     s.sendto(str(port).encode(),dir)
+    print("UDP server up and listening at port ",port)
 
-    #Start server
-    while True: 
-        data = s.recvfrom()
-        message  = data[0]
+    while True:
+        data = s.recvfrom(BUFFER)
+        msg = data[0]
         dir = data[1]
 
-        print("Server received", message.decode())
+        print('Server received', msg.decode())
 
-        if(message.decode() == "LISTENING!):
-            connected_customers += 1
-            print("Customers connected: ", connected_customers)
-            encrypHash = hashlib.sha1()
-
+        if (msg.decode() == "READY"):
+            c_clientes += 1
+            print("Numero Clientes Conectados: ", c_clientes)
+            sha1 = hashlib.sha1()
             while True:
-                if(connected_customers >= number_clients or attend ):
+                if (c_clientes >= n_clientes or attend):
                     print("Starting to send")
                     break
-            #The server is on and listening
-            attend =  True
+            attend = True
             i = 0
-            s.sendto(fileToTransfer.encode(),dir)
+
+            s.sendto(fileT.encode(), dir)
 
             time.sleep(0.01)
 
-            startTransfer  = time.time()
-            #File to transfer
-            f = open(fileName, 'rb') 
-            while True: 
-                i+= 1 
+            inicioT = time.time()
+            f = open(fileName, 'rb')
+            while True:
+                i += 1
                 data = f.read(BUFFER)
-                if not data: 
+                if not data:
                     break
-                encrypHash.update(data)
+                sha1.update(data)
                 s.sendto(data,dir)
-            print("Send File")
+            print("Archivo Enviado")
 
-            #Create Hash 
-            hass =  str(encrypHash.hexdigest())
-            s.sendto(("FINM"+has).encode(),dir)
+            # Envio de Hash
+            has = str(sha1.hexdigest())
+            s.sendto(("FINM" + has).encode(),dir)
             f.close()
 
+
             data = s.recvfrom(BUFFER)
+            # Notificacion de recepcion
+            datosCiente = data[0].decode().split("/")
+            recepcion = datosCiente[1]
+            print(recepcion)
 
-            #Notification of recive
-            datosCliente  = data[0].decode().split("/")
-            reception = datosCliente[1]
-            print(reception)
+            # Notificacion de tiempo
+            finT = float(datosCiente[2])
+            totalT = finT - inicioT
+            # print("Tiempo total:",totalT, "Segundos")
 
-            #Time notification
-            endToTransfer = float(datosCliente[2])
-            totalTransfer = endToTransfer -startTransfer
+            # Numero de paquetes recibidos por el cliente
+            paqRecv = datosCiente[0]
 
-            #How many packages has the client recive
-            packClientRecive = datosCliente[0]
-            hasofRecive = datosCliente[4]
+            hashR = datosCiente[4]
+            logDatosCliente(recepcion, totalT, i, paqRecv, hashR, has)
 
-            #LLAMAR METODO DE CLIENTE
+            print('Fin envio')
+            c_clientes -= 1
+            print("Numero Clientes Conectados: ", c_clientes)
 
-            print('End sending')
-            connected_customers -=1
-            print("Customers connected: ", connected_customers)
+            # Notificacion de fin de cliente o no
+            terminS = datosCiente[3]
+            # print("Mensaje del cliente: ", terminS)
 
-            #End or not notification
-            endSesion = datosCliente[3]
-
-            if(endSesion == "FINISH"):
-                print(endSesion)
+            if (terminS == "TERMINATE"):
+                print(terminS)
                 s.close()
-                print("End server on port", port)
+                print("Fin Servidor en puerto ",port)
                 break
 
 
-port_1 = 20001
+
+port1=20001
+
+# UDP ------> socket.AF_INET, socket.SOCK_DGRAM
 s = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
 # Bind to address and ip
-s.bind((host, port_1))
+s.bind((host, port1))
 i=1
 while True:
     data = s.recvfrom(BUFFER)
@@ -140,13 +180,3 @@ while True:
     if (msg.decode() == "END"):
         print("FIN CONEXIONES")
         break
-
-
-
-
-
-
-
-
-    
-    
